6c68f9350186d8356f0d1bc65393340110fddac1
MaxPtg <max.patecky@htw-dresden.de>
2024-08-31 17:27:23 +0200

    updated tests

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-DOCKER_BUILDKIT=1 docker build --build-arg BUILDKIT_INLINE_CACHE=1 -t debian12-custom . && docker run -it --rm debian12-custom
+DOCKER_BUILDKIT=1 docker build \
+  --build-arg BUILDKIT_INLINE_CACHE=1 \
+  -t deb12-test-container . && \
+docker run -it --rm deb12-test-container
